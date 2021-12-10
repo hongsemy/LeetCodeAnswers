9ad637a2315deff5191a39f60cf7405120c0c67b
hongsemy <semyeong.hong@mail.utoronto.ca>
2021-12-10 17:17:42 +0900

    feat: Update solution for q2 (Done)

--- answers/leetcode2.py
+++ answers/leetcode2.py
@@ -27,26 +27,31 @@ def add_two_numbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[
     Return
         Optional[ListNode]: [description]
     """
-    first_sum = l1.val + l2.val
+    result = ListNode()
+    working_node = result
     extra = 0
-    if first_sum > 9:
-        extra = int(str(first_sum)[0])
-        first_sum = int(str(first_sum)[1])
-    working_node = ListNode()
-    final_node = ListNode(first_sum, working_node)
-    while l1.next is not None and l2.next is not None:
-        l1, l2 = l1.next, l2.next
-        sum = l1.val + l2.val + extra
+    while l1 is not None or l2 is not None:
+        if l1 is None:
+            val_l1 = 0
+        else:
+            val_l1 = l1.val
+            l1 = l1.next
+        if l2 is None:
+            val_l2 = 0
+        else:
+            val_l2 = l2.val
+            l2 = l2.next
+        sum = val_l1 + val_l2 + extra
         if sum > 9:
             extra = int(str(sum)[0])
             sum = int(str(sum)[1])
         else:
             extra = 0
-        working_node.val = sum
-        new_node = ListNode()   # This line causes an extra redundent node
-        working_node.next = new_node
-        working_node = new_node
-    return final_node
+        working_node.next = ListNode(sum)
+        working_node = working_node.next
+    if extra != 0:
+        working_node.next = ListNode(extra)
+    return result.next
         
         
 def print_nodes(l):
@@ -56,9 +61,11 @@ def print_nodes(l):
         l = l.next
     num = str(l.val) + num
     print(num)
-    
+
+
 if __name__ == '__main__':
-    l1_3 = ListNode(3)
+    l1_4 = ListNode(2)
+    l1_3 = ListNode(5, l1_4)
     l1_2 = ListNode(4, l1_3)
     l1_1 = ListNode(2, l1_2)
     l2_3 = ListNode(4)
